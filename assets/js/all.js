d3aaecd7c02a7893d8997c6c5aecc9745406d8d9
lily <a0976198586@gmail.com>
2022-05-30 09:34:33 +0800

    Update 2022-05-30T01:34:31.887Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -143,7 +143,7 @@ function changeBtn(input) {
 
   resultBtn.style.display = 'none'; //隱藏
 
-  showResult.className = ''; //將之前的按鈕顏色style設為空
+  showResult.className = ''; //將之前的按鈕顏色style設為空(之前的除錯處)
 
   showResult.classList.add(color); //再重新賦了新的按鈕顏色
 
